e01ac0d679aa216062e1368deddca49eb5edb51c
lancedussault <144727107+lancedussault@users.noreply.github.com>
2024-08-23 14:11:12 -0600

    Got the spacecraft to move and now working on the asteroid bit

---
+++ asteroid.py
@@ -0,0 +1,10 @@
+import pygame
+from constants import *
+from circleshape import CircleShape
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, 'white', self.radius, 2)
